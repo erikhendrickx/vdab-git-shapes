14267a1071e9c6407b631d6c7863342d11169dc5
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-05-18 17:04:49 +0200

    Added instructions

--- src/main/java/be/vdab/shapes/Main.java
+++ src/main/java/be/vdab/shapes/Main.java
@@ -2,7 +2,18 @@ package be.vdab.shapes;
 
 /**
  * Exercise:
- *  - Organise yourselves in teams of two members
+ *  - Organise yourselves in teams of two members<br/><br/>
+ *  - Start from the same (shared) repository version<br/><br/>
+ *  - Team member one:<br/>
+ *      * Add a new abstract method "double Shape.area()". Make sure all subclasses implement the contract.<br/>
+ *      * Modify Shape.toString() to also print out the area of the shape ("Shape 'Triangle' has an area of 12 square units")<br/><br/>
+ *  - Team member two:<br/>
+ *      * Add a new abstract method "double Shape.perimeter()". Make sure all subclasses implement the contract.<br/>
+ *      * Modify Shape.toString() to also print out the perimeter of the shape ("Shape 'Triangle has a perimeter of 16 units")<br/><br/>
+ * - Make sure you commit these changes to the local repository<br/><br/>
+ * - Push the changes to the origin<br/>
+ *      * Do you encounter any problems? What about your team member?<br/>
+ *      * Who was the first to push his/her changes to the origin?<br/>
  */
 public class Main {
     private static Shape[] shapes = {
